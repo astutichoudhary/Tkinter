decdbb06bd7a077a0ce1aff746af38bac4c932d0
astutichoudhary <astutichoudhary31@gmail.com>
2026-07-09 21:34:15 +0530

    ..

--- file.py
+++ file.py
@@ -1,16 +1,20 @@
 from tkinter import *
-from PIL import Image,ImageTk
+
+from PIL import Image,ImageTk # to avoid the error coming in accessing jpg file
+
 window = Tk()
 
 window.geometry("720x480")
 window.title("Astuti's window")
 
-# icon = PhotoImage(file ='meriphoto.jpg')
+# CANNOT FIGURE OUT HOW TO ADD PHOTO IMAGE AND ICON. WILL COME BACK LATER.
+
+# icon = PhotoImage(file ='photo.png')
 # window.iconphoto(True,icon)
 
-opened_img = Image.open("meriphoto.jpg")
-img = ImageTk.PhotoImage(opened_img)
-window.iconphoto(True,img)
+# opened_img = Image.open("meriphoto.jpg")
+# img = ImageTk.PhotoImage(opened_img)
+# window.iconphoto(True,img)
 
 window.config(background='Sky Blue')
 window.mainloop()
